6caa838571d2eaf3fa588084047a42f32b1958ce
MSI\Mia <02thuhabui@gmail.com>
2023-11-20 16:17:32 +0700

    again

--- Screens/index.php
+++ Screens/index.php
@@ -7,23 +7,30 @@
     <link rel="stylesheet" href="../Style/login-style.css"> 
 </head>
 <body>
-    <div id="login-container" style=" width: 100%; height: 100%">
-        <div class="container-content">
-            <div class="container-content-left">
-                <img src="../Assets/LoginBox.png"/>
-            </div>
-            <div class="container-content-right">
-                <p class="title" style="color: #046543; font-size: 20px;margin-top: 20px;"><b>CHÀO MỪNG ĐẾN VỚI MIA BOOKCORNER</b></p>
-                <p class="title" style="color: #9ba9a4; font-size: 14px">Đăng nhập tài khoản của bạn</p>
-                <p class="title" style="margin-top: 30px; font-size: 16px"><b>Số điện thoại</b></p>
-                <input style="width: 323px; height: 31px" />
-                <p class="title" style="margin-top: 30px; font-size: 16px"><b>Mật khẩu</b></p>
-                <input style="width: 323px; height: 31px" />
-                <button class="button"><a href="home-screen.html" style="color: #F6C017; padding: 10px; text-decoration: none;"><b>ĐĂNG NHẬP</b></a></button>
-                <p class="title" style="color: #9ba9a4; margin-top: 10px; margin-left:78px; font-style: italic; font-size: 14px">Chưa có tài khoản? <a href="signup.html">Đăng ký</a> </p>
+    <form action="login.php" method="post">
+        <div id="login-container" style=" width: 100%; height: 100%">
+            <div class="container-content">
+                <div class="container-content-left">
+                    <img src="../Assets/LoginBox.png"/>
+                </div>
+                <div class="container-content-right">
+                    <p class="title" style="color: #046543; font-size: 20px;margin-top: 20px;"><b>CHÀO MỪNG ĐẾN VỚI MIA BOOKCORNER!</b></p>
+                    <p class="title" style="color: #9ba9a4; font-size: 14px">Đăng nhập tài khoản của bạn</p>
+
+                    <?php if (isset($_GET['error'])) { ?>
+     		            <p class="error"><?php echo $_GET['error']; ?></p>
+     	            <?php } ?>
+
+                    <p class="title" style="color: #046543;margin-top: 30px; font-size: 16px"><b>Số điện thoại</b></p>
+                    <input type="text" name="uname" placeholder="User Name" style="width: 323px; height: 31px" />
+                    <p class="title" style="color: #046543;margin-top: 30px; font-size: 16px"><b>Mật khẩu</b></p>
+                    <input type="password" name="password" placeholder="Password" style="width: 323px; height: 31px" />
+                    <button class="button" style="color: #F6C017; padding: 10px; text-decoration: none;"><b>ĐĂNG NHẬP</b></a></button>
+                    <p class="title" type="submit" style="color: #9ba9a4; margin-top: 10px; margin-left:115px; font-style: italic; font-size: 14px">Chưa có tài khoản? <a href="signup.php">Đăng ký</a> </p>
+                </div>
             </div>
         </div>
-    </div>
+    </form>
 </body>
 </html>
 
